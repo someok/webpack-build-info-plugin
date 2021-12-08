b4d0b47607e27673bdf15376ecd575a8a353eb9a
JX Wang <wjianxu@gmail.com>
2021-12-08 16:47:04 +0800

    feat: 增加 outputOsInfo 属性用于配置是否输出 os 信息

--- src/index.js
+++ src/index.js
@@ -5,51 +5,70 @@ const path = require('path');
 const PLUGIN_NAME = 'WebpackBuildInfoPlugin';
 
 class WebpackBuildInfoPlugin {
-
-    constructor(options = {
-        outputFilename: 'build-info.json',
-        outputGitInfo: true,
-        extraBuildProperties: {}
-    }) {
+    constructor(
+        options = {
+            outputFilename: 'build-info.json',
+            outputGitInfo: true,
+            outputOsInfo: true,
+            extraBuildProperties: {},
+        }
+    ) {
         this.options = options;
     }
 
     apply(compiler) {
-        const outputPath = compiler.options.output && compiler.options.output.path;
+        const outputPath =
+            compiler.options.output && compiler.options.output.path;
 
         if (!outputPath) {
-            console.warn(PLUGIN_NAME, ': `options.output.path` not defined. Plugin DISABLED!');
+            console.warn(
+                PLUGIN_NAME,
+                ': `options.output.path` not defined. Plugin DISABLED!'
+            );
             return;
         }
 
         compiler.hooks.done.tap(PLUGIN_NAME, (stats) => {
-            const {hash, startTime, endTime} = stats;
+            const { hash, startTime, endTime } = stats;
 
             const gitInfo = {};
             if (this.options.outputGitInfo) {
                 try {
-                    const commitHash = require('child_process').execSync('git rev-parse HEAD');
+                    const commitHash =
+                        require('child_process').execSync('git rev-parse HEAD');
 
                     gitInfo.commitHash = commitHash.toString().trim();
                 } catch (e) {
                     console.warn(e);
                 }
-
             }
 
-            const buildInfo = Object.assign({}, {
-                hash,
-                startTime,
-                endTime,
-                osInfo: {
+            const osInfo = {};
+            if (this.options.outputOsInfo) {
+                osInfo.osInfo = {
                     arch: os.arch(),
                     platform: os.platform(),
                     release: os.release(),
                     type: os.type(),
                     username: os.userInfo().username,
+                };
+            }
+
+            const buildInfo = Object.assign(
+                {},
+                {
+                    hash,
+                    startTime,
+                    endTime,
                 },
-            }, gitInfo, this.options.extraBuildProperties);
-            fs.writeFileSync(path.resolve(outputPath, this.options.outputFilename), JSON.stringify(buildInfo, null, 4));
+                gitInfo,
+                osInfo,
+                this.options.extraBuildProperties
+            );
+            fs.writeFileSync(
+                path.resolve(outputPath, this.options.outputFilename),
+                JSON.stringify(buildInfo, null, 4)
+            );
         });
     }
 }
